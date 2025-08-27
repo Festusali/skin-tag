262bfb2c5aee225f89dc5eebefebe487d663b029
Festus Ali <festusali@gmail.com>
2025-08-27 16:23:22 +0100

    Change Skin Tag Product Prices 15th

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,17 @@
 
 // Assigns corresponding value to price based on product quantity
 function checkForm() {
-    orderQty = document.orderForm.quantity
-    if (orderQty.value == 2) {
-        document.orderForm.price.value = "₦20,000";
-        return true;
-    }
-    if (orderQty.value == 4) {
-        document.orderForm.price.value = "₦37,000";
-        return true;
-    }
-    if (orderQty.value == 6) {
-        document.orderForm.price.value = "₦55,500";
-        return true;
-    }
-    if (orderQty.value == 8) {
-        document.orderForm.price.value = "₦74,000";
-        return true;
-    }
-    if (orderQty.value == 10) {
-        document.orderForm.price.value = "₦92,500";
-        return true;
-    }
+  orderQty = document.orderForm.quantity;
+  if (orderQty.value == 2) {
+    document.orderForm.price.value = "₦18,500";
+    return true;
+  }
+  if (orderQty.value == 4) {
+    document.orderForm.price.value = "₦30,000";
+    return true;
+  }
+  if (orderQty.value == 6) {
+    document.orderForm.price.value = "₦60,000";
+    return true;
+  }
 }
