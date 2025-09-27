d2233f164e418feba328e69cbd717019412c5676
Festus Ali <festusali@gmail.com>
2025-09-27 12:42:53 +0100

    Change Skin Tag Product Prices 17th

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ function checkForm() {
     return true;
   }
   if (orderQty.value == 6) {
-    document.orderForm.price.value = "₦60,000";
+    document.orderForm.price.value = "₦50,000";
     return true;
   }
 }
